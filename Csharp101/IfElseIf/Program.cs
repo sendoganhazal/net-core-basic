37f02b759cff2f1f03d5f16fb3800998037c7a1a
Hazal Sendogan <sendoganhazal@gmail.com>
2026-04-03 14:27:12 +0300

    if - else if -ternary

---
+++ Csharp101/IfElseIf/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Derste Öğrenilenler");
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time <= 11)
+              Console.WriteLine("Günaydın");
+            else if (time <= 18)
+              Console.WriteLine("İyi Günler");
+            else
+              Console.WriteLine("İyi Geceler");
+
+            string sonuc = time >= 6 && time <= 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler"; // Ternary if-else
+            Console.WriteLine("ternary: " + sonuc);
+        }
+    }
+}
+
