e4d8e7a7d97b03c5a6fc7c36e989f42cdea100a0
Hazal Sendogan <sendoganhazal@gmail.com>
2026-04-04 15:54:41 +0300

    Array Tanımlama, Erişme ve Döngülerle Kullanımı

--- Csharp101/Arrays/Program.cs
+++ Csharp101/Arrays/Program.cs
@@ -21,6 +21,39 @@ namespace Arrays
         }
         public static void DefineArray()
         {
+            //** Defining arrays
+            string[] colors = new string[5];
+            string[] animals = { "Cat", "Dog", "Bird", "Monkey" };
+
+            int[] arr;
+            arr = new int[5];
+
+            // adding values to array and accessing them
+            colors[0] = "Red";
+            Console.WriteLine(animals[1]);
+            arr[3] = 10;
+            Console.WriteLine(colors[0]);
+            Console.WriteLine(arr[3]);
+
+            //using for loop to access array elements
+            // Klavyeden girilen n tane sayının ortalamasını hesaplayan program
+            Console.WriteLine("Lütfen dizinin eleman sayisini giriniz:");
+            int diziUzunluk = int.Parse(Console.ReadLine());
+            int[] sayiDizi = new int[diziUzunluk];
+
+            for (int i = 0; i < diziUzunluk; i++)
+            {
+                Console.WriteLine($"Lütfen {i + 1}. sayıyı giriniz:");
+                sayiDizi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (int sayi in sayiDizi)
+            {
+                toplam += sayi;
+            }
+
+            Console.WriteLine("Ortalama:" + "" + toplam / diziUzunluk);
         }
         public static void MethodsArray()
         {
--- Csharp101/Arrays/obj/Debug/net10.0/Arrays.AssemblyInfo.cs
+++ Csharp101/Arrays/obj/Debug/net10.0/Arrays.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e6acaf0131a3e56a7a94b3533065af3cdfaf5e")]
-[assembly: System.Reflection.AssemblyProductAttribute("Variables")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
+[assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
