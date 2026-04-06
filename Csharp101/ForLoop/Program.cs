f4e7b688a3aeb0dd3286fcc8018e35799bc4348a
Hazal Sendogan <sendoganhazal@gmail.com>
2026-04-06 10:30:06 +0300

    for Loop and break continue

---
+++ Csharp101/ForLoop/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Derste Öğrenilenler");
+
+            //** For Loop
+            Console.WriteLine("***** For Loop *****");
+            ForLoop();
+            //** Break
+            Console.WriteLine("***** Break *****");
+            Break();
+            //** Continue
+            Console.WriteLine("***** Continue *****");
+            Continue();
+        }
+        public static void ForLoop()
+        {
+            // 0'dan console'dan girilen sayıya kadar olan tek sayıları ekrana yazdıran bir for döngüsü yazınız.
+            Console.Write("Lütfen Bir Sayi Giriniz:");
+            int sayac = int.Parse(Console.ReadLine());
+            for (var i = 0; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                    Console.WriteLine(i);
+            }
+
+            //1'den 1000'e kadar olan sayılardan teklerin ve çiftlerin kendi içlerindeki toplamını ekrana yazdıran bir for döngüsü yazınız.
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (var i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                    tekToplam += i; //teklerin kendi içlerindeki toplamı
+                else
+                    ciftToplam += i; //çiftlerin kendi içlerindeki toplamı
+
+            }
+            Console.WriteLine("Teklerin Toplami: " + tekToplam);
+            Console.WriteLine("Ciftlerin Toplami: " + ciftToplam);
+        }
+
+        public static void Break()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                    break; //i 4'e eşit olduğunda döngüyü kırar ve döngüden çıkar.
+                Console.WriteLine(i);
+            }
+
+        }
+
+        public static void Continue()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                    continue; //i 4'e eşit olduğunda o adımı atlar ve döngünün bir sonraki adımına geçer.
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+
+
--- Csharp101/ForLoop/obj/Debug/net10.0/ForLoop.AssemblyInfo.cs
+++ Csharp101/ForLoop/obj/Debug/net10.0/ForLoop.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ForLoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c6dad78076d492ed77ba78f1754ddf7a59924b")]
-[assembly: System.Reflection.AssemblyProductAttribute("Variables")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
+[assembly: System.Reflection.AssemblyProductAttribute("ForLoop")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ForLoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
