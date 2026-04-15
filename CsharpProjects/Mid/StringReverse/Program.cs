94e9d83eb79cc71cf982ec65e741c540fd6fb463
Hazal Sendogan <sendoganhazal@gmail.com>
2026-04-15 15:59:45 +0300

    Karakter Değiştirme

---
+++ CsharpProjects/Mid/StringReverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir ifade giriniz:");
+        string input = Console.ReadLine();
+
+        string[] kelimeler = input.Split(' ');
+
+        for (int i = 0; i < kelimeler.Length; i++)
+        {
+            string kelime = kelimeler[i];
+
+            if (kelime.Length > 1)
+            {
+                char ilk = kelime[0];
+                char son = kelime[kelime.Length - 1];
+                string orta = kelime.Substring(1, kelime.Length - 2);
+
+                kelimeler[i] = son + orta + ilk;
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", kelimeler));
+    }
+}
