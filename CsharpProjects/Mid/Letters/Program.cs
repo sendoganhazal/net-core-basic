9c8852b192a10082356d7ff555cfa48a676088f5
Hazal Sendogan <sendoganhazal@gmail.com>
2026-04-15 16:12:33 +0300

    sessiz harf algoritması

---
+++ CsharpProjects/Mid/Letters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir ifade giriniz:");
+        string input = Console.ReadLine().ToLower();
+
+        string[] kelimeler = input.Split(' ');
+
+        foreach (string kelime in kelimeler)
+        {
+            Console.Write(CheckConsonants(kelime) + " ");
+        }
+    }
+
+    static bool CheckConsonants(string kelime)
+    {
+        string sessizler = "bcçdfgğhjklmnprsştvyz";
+
+        for (int i = 0; i < kelime.Length - 1; i++)
+        {
+            if (sessizler.Contains(kelime[i]) && sessizler.Contains(kelime[i + 1]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
